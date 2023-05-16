5bba050bce697d3bfcf1c5cbb61b99c2c05dcc7b
t.onodera <t.onodera.1.2.1.5@gmail.com>
2023-05-16 07:42:58 +0000

    「sea-orm-cli migrate up」でテーブルの更新を適用できるの確認。

---
+++ app/migration/src/m20230516_073733_add_created_at_and_updated_at_to_table.rs
@@ -0,0 +1,50 @@
+use sea_orm_migration::prelude::*;
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .alter_table(
+                Table::alter()
+                    .table(Post::Table)
+                    .add_column(
+                        ColumnDef::new(Post::CreatedAt)
+                            .timestamp_with_time_zone()
+                            .not_null(),
+                    )
+                    .add_column(
+                        ColumnDef::new(Post::UpdatedAt)
+                            .timestamp_with_time_zone()
+                            .not_null(),
+                    )
+                    .to_owned(),
+            )
+            .await
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .alter_table(
+                Table::alter()
+                    .table(Post::Table)
+                    .drop_column(Post::CreatedAt)
+                    .drop_column(Post::UpdatedAt)
+                    .to_owned(),
+            )
+            .await
+    }
+}
+
+/// Learn more at https://docs.rs/sea-query#iden
+#[derive(Iden)]
+enum Post {
+    Table,
+    Id,
+    Title,
+    Text,
+    CreatedAt,
+    UpdatedAt,
+}
